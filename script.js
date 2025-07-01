9d6fd241a0803a26bb3c76836a142b91dc392d26
yatziel <89828295+yatziel@users.noreply.github.com>
2025-07-01 10:57:05 -0600

    Cambio dinámico de icon-theme y marca grafica

--- script.js
+++ script.js
@@ -2,9 +2,16 @@
 
 const toggleButton = document.getElementById('toggle-button');
 const body = document.body;
+const logo = document.getElementById('logo');
 
 /* Creación una función para ejecutar un código */
 
 toggleButton.addEventListener('click', () => {
   body.classList.toggle('dark');
+
+  // Cambiamos el logo según el tema
+
+  const isDark = body.classList.contains('dark'); // true - false
+
+  logo.src = isDark ? './assets/marca-grafica-dark.svg' : './assets/marca-grafica.svg';
 });
